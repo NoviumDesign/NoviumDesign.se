96671d8466beac67d5fc3c6e055c874cb766e197
Anton Kjörck Lindén <anton@noviumdesign.se>
2013-03-15 13:57:33 +0100

    New strategy page

--- nav.php
+++ nav.php
@@ -1,22 +1,23 @@
 <header>
 	<div class="container">
 		<div class="row">
-			<div class="span6 logo">
+			<div class="span5 logo">
 				<a href="/"><img src="/img/logo.png" alt="Novium Designbyrå"></a>
 			</div>
-			<div class="span6 navigation">
+			<div class="span7 navigation">
 				<nav class="clearfix">
 					<ul class="nav">
 						<li><a href="/">Start</a></li>
 						<li><a href="/portfolio">Portfolio</a></li>
-						<li class="hidden-phone"><a href="/contact">Kontakta oss</a></li>
-						<li class="visible-phone"><a href="/contact">Kontakt</a></li>
+						<li><a href="/strategy">Erbjudande</a></li>
+						<li class="visible-desktop"><a href="/contact">Kontakta oss</a></li>
+						<li class="hidden-desktop"><a href="/contact">Kontakt</a></li>
 					</ul>
 				</nav>
 			</div>
 		</div>
 		<div class="row leadrow">
-			<div class="span6 visible-desktop">
+			<div class="span5 visible-desktop">
 				<h2>
 					<ul class="unstyled activities">
 						<li>Branding</li>
@@ -25,8 +26,8 @@
 					</ul>
 				</h2>
 			</div>
-			<div class="span6 ingres">
-				<h2>Novium är en designbyrå. Med teknik och estetik hjälper vi varumärken att synas i mängden och bli valda.</h2>
+			<div class="span7 ingres">
+				<h2>Novium är en varumärkes- och designbyrå. Med stategi, teknik och branding hjälper vi varumärken att skilja sig från mängden och bli valda.</h2>
 			</div>
 		</div>
 	</div>
---
+++ strategy.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="lt-ie9 lt-ie8 lt-ie7" lang="sv"> <![endif]-->
+<!--[if IE 7]>         <html class="lt-ie9 lt-ie8" lang="sv"> <![endif]-->
+<!--[if IE 8]>         <html class="lt-ie9" lang="sv"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="sv"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<link rel="dns-prefetch" href="http://ajax.googleapis.com" />
+		<link rel="dns-prefetch" href="http://www.google-analytics.com/ga.js" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Novium Designbyrå - Strategi</title>
+
+		<meta name="description" content="Läs om hur vi arbetar med att bygga varumärken.">
+		<meta name="author" content="">
+		<meta name="keywords" content="Designbyrå, Novium Designbyrå, Novium Design, Förpackningsdesign, Logotype, Logotyp">
+
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="sitemap" type="application/xml" title="Sitemap" href="sitemap.xml">
+
+		<meta property="og:title" content="Novium Designbyrå">
+		<meta property="og:description" content="Läs om hur vi arbetar med att bygga varumärken.">
+		<meta property="og:url" content="http://noviumdesign.se">
+
+		<!--[if lt IE 9]>
+			<script src="js/vendor/html5-3.6-respond-1.1.0.min.js"></script>
+		<![endif]-->
+	</head>
+	<body class="strategy-page">
+		<?php include('nav.php') ?>
+		<div class="container">
+			<div class="offer">
+				<div class="row">
+					<div class="span4">
+						<ul class="unstyled">
+							<li><h5>Strategi</h5></li>
+							<li>
+								Genom research och nya insikter skapar vi en tydlig riktning för varumärket.
+							</li>
+							<li><h5>Branding</h5></li>
+							<li>
+								Utveckling av designkoncept som förstärks med grafiska detaljer.
+							</li>
+							<li><h5>Teknik</h5></li>
+							<li>
+								Vi tar ett helhetsansvar och använder oss av senaste teknikerna.
+							</li>
+						</ul>
+					</div>
+					<div class="offset1 span4">
+						<ul class="unstyled">
+							<li>Analys & research</li>
+							<li>Varumärkesstrategi</li>
+							<li>Konceptualisering</li>
+							<li>Ikon & gränsnittsdesign</li>
+							<li>Grafisk identitet</li>
+							<li>Förpackningsdesign</li>
+							<li>Webdesign & sociala medier</li>
+							<li>Publiceringssystem & serverhantering</li>
+							<li>Affärsapplikationer</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php include('footer.html') ?>
+
+	</body>
+</html>
